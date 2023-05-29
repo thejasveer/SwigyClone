c514997111aabde39ddb7aaeade759d270ce4400
Jasveer <thejasveer13@gmail.com>
2023-05-28 21:07:32 -0500

    Comleted Episode 3

--- App.js
+++ App.js
@@ -2,23 +2,51 @@ import React from'react';
 import ReactDOM from'react-dom/client';    
     
     
-    const parent = React.createElement(
-        "div"
-        ,{id:"parent"},
-        React.createElement(
-            "div",
-             {"id":"child"},
-             [
-                React.createElement("h1",{},"I sam h1 js tag"),
-                React.createElement("h2",{},"I am h2 tag"),
+//JSX - Its not html , it is html-like or XML-like syntax
+// JSX is first transpiled before it reahces JS - Parcel - Babel is resposible for this
+// JSX => React.createElement => ReactElement-JS Obeject => HTMLElement(Render)
+// do attributes as camel-case
 
-        ]
-    
-    ));
-    
-    
-    //React.createElement is creating new element using react cdn
-    const heading = React.createElement("h1",{"id":"heading"},"Hello world from React");
-    //ReactDOM is responsible for manipulating the DOM
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(parent);
+const elem = <span>Recat Element</span>;
+const Title =(
+    <div id='container'> 
+
+    <h1  className='heading'>Hi From React 
+
+    </h1>
+</div> 
+
+
+);
+console.log(Title);
+
+
+/* 
+React Components
+1. Class Based Components - OLD
+2. Functional Components - NEW 
+
+*/
+
+let number = 10000;
+
+// Functional Component => render use <HeadingComponent/>
+var HeadingComponent = () => (
+    //React Fragments
+    <React.Fragment> // <>   </>
+  <div id='container'> 
+   {/* Component Composition - When you composing two components in each otherß  */}
+    {Title}   
+    <h2>{number}</h2>
+    <h1>This is Heading Component</h1>
+    </div>
+    <div id='container-2'> 
+   {/* Component Composition - When you composing two components in each otherß  */}
+    {Title}   
+    <h2>{number}</h2>
+    <h1>This is Heading Component</h1>
+    </div>
+    </React.Fragment>
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HeadingComponent/>);
