ef3f72a2b9bd5126a43977beb6ac222603e35371
Jasveer <thejasveer13@gmail.com>
2023-08-15 15:52:53 -0500

    Added hof

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,4 @@
-import ResCard from "./ResCard";
+import ResCard, {WithOpenLabel} from "./ResCard";
 import { useEffect, useState } from "react";
 import { Shimmer } from "./Shimmer";
 import { Link } from "react-router-dom";
@@ -11,15 +11,15 @@ const Body = ()=>{
     const [resList,setResList] = useState([]);
     const [filteredRes,setFilteredResList]=useState([])
     const [searchInput,setSearchInput] = useState("");
-
+    const OpenRes = WithOpenLabel(ResCard);
     useEffect(()=>{
        fetchData();
     },[]);
     const fetchData = async ()=>{
         const data1 = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&page_type=DESKTOP_WEB_LISTING");
         const {data} = await data1.json();
-
-        const resListData = await data?.cards[2]?.data?.data?.cards;
+        const resListData =  data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+      
         setResList(resListData)
         setFilteredResList(resListData)
          
@@ -49,7 +49,7 @@ const Body = ()=>{
             
         </div>
          <button className="filter-btn" onClick={()=>{
-            filteredList = resList.filter((res => res.data.avgRating>4.3))
+            filteredList = resList.filter((res => res?.info?.avgRating>4.3))
             setFilteredResList(filteredList)
            }}>Top Rated Restraunts</button>
       </div>
@@ -58,9 +58,10 @@ const Body = ()=>{
       {
           // resList.map(res=> <ResCard resData={res}/>)
           filteredRes.map((res)=>(
-            <Link to="/res/{res.data.id}"  key={res.data.id}> <ResCard
-             
-              resData={res}/>
+            <Link to="/res/{res?.info.id}"  key={res?.info.id}> 
+          
+              {res.info.avgRating>4 ? <OpenRes resData={res?.info}/>:<ResCard resData={res?.info}/>}
+              
               </Link> 
           ))
       }
@@ -70,5 +71,5 @@ const Body = ()=>{
   </div>
     ) 
   }
-  export const a="Ss";
+
   export default Body;
--- src/components/ResCard.js
+++ src/components/ResCard.js
@@ -2,15 +2,15 @@ import {CDN_URL} from '../utils/constants';
 import { Link } from "react-router-dom";
 const ResCard = (props)=>{ 
     const {resData}= props;
-    const {name,cuisines,avgRating,costForTwo}= resData?.data;
+    const {name,cuisines,avgRating,costForTwo}= resData;
  return (
-    <Link to={  `/res/${resData.data.id}`} >
+    <Link to={  `/res/${resData.id}`} >
            <div className='res_card'>
-        <img src={CDN_URL+resData.data.cloudinaryImageId} />
+        <img src={CDN_URL+resData.cloudinaryImageId} />
         <h3 className='title'>{name}</h3>
         <h4>{cuisines.join(', ')}</h4>
         <h4>{avgRating} ⭐️ </h4>
-        <h4>${costForTwo/100} For Two</h4>
+        <h4>{costForTwo}</h4>
         <p></p>
     </div>
     </Link>
@@ -18,4 +18,19 @@ const ResCard = (props)=>{
  );
 };
 
+// higher order component
+
+//input 
+export const WithOpenLabel  = (ResCard) =>{
+   return (props)=>{
+      const {resData}= props;
+      return (
+         <div style={{position:'relative'}}>
+            <label style={{position:'absolute',background:'#000',color:'#fff',top:'0',padding:'5px',borderRadius:'5px'}}>Promoted</label>
+            <ResCard resData={resData}/>
+         </div>
+      )
+
+   }
+}
 export default ResCard;     
